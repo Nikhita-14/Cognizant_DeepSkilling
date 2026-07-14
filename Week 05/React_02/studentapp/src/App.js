128c9010e718a192a71ac1c228742e72dbd65d16
Nikhita-14 <dittakavinikhita@gmail.com>
2026-07-14 19:46:15 +0530

    Week 05

---
+++ Week 05/React_02/studentapp/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+        <h1>Student Management Portal</h1>
+        <hr />
+        <Home />
+        <About />
+        <Contact />
+      </div>
+    );
+  }
+}
+
+export default App;
